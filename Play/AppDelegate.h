8cade82e0c20a721024bd0c5e96b115185e2e509
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2015-09-14 08:01:57 +0930

    Initial Commit

---
+++ Play/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Play
+//
+//  Created by Aidan Cornelius-Bell on 14/09/2015.
+//  Copyright © 2015 Teacher Solutions Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
